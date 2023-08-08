981b5f9d5ecd11b6d364261201a2a14172c64465
Lionel <lionel@morrison.mobi>
2023-08-08 17:47:40 -0400

    Initial mini-resume

---
+++ app/data.ts
@@ -0,0 +1,63 @@
+import { Job } from './interfaces';
+
+export const jobsData: Job[] = [
+  {
+    id: 'UA',
+    company: 'Under Armour',
+    title: 'Sr. Software Engineer',
+    dates: { start: '2022-09-12', end: '2023-06-16' },
+    tldr: 'Summary goes here',
+    highlights: [
+      'Lead global deployment rollouts and hotfixes working alongside developers and QA engineers to identify non-production-ready items',
+      'Instrumental in new search engine implementation: $30M sales lift, 50% faster search, and improved accuracy',
+      'Proficiently identified, logged, and resolved issues',
+      'Actively engaged in standups and proficiently utilized Jira for recording and tracking work',
+      'Collaborated with creative translating UI layouts into functional code implementations',
+      'Conducted peer reviews of code for both contractors and full-time employees',
+      'Utilized SFRA for crafting high-quality, robust code',
+    ],
+  },
+  {
+    id: 'NI',
+    company: 'Nivel Parts and Manufacturing',
+    title: 'Web Application Developer',
+    dates: { start: '2021-08-12', end: '2022-08-12' },
+    highlights: [
+      'Implemented Scrum into development, marketing, and design teams',
+      'Implemented Git and CircleCI for code tracking, code review, and deployment',
+      'Introduced TypeScript and Git as standards for new development',
+      'Created serverless Lambda API for data consumption from KiboCommerce',
+      'Collaborated cross-functionally for UI/UX and SEO enhancements',
+      // eslint-disable-next-line quotes
+      "Architected serverless API to synchronization 3rd party APIs with companies' source of truth",
+    ],
+  },
+  {
+    id: 'RL',
+    company: 'Redleaf Software',
+    title: 'Web Application Developer',
+    dates: { start: '2018-11-01', end: '2021-08-12' },
+    highlights: [
+      // eslint-disable-next-line quotes
+      "Led greenfield Angular development for Importing revenue management files into a data-focused SPA dashboard providing market intelligence to optimize hoteliers' revenue through data analysis, forecasts, and budgets",
+      'Significantly decreased report generation time by 60-75% and achieved exponential accuracy improvements freeing up employee time',
+      'Proactively collaborated with product owners to drive UI/UX design and implementation using Adobe XD',
+      'Conducted unit testing to achieve code coverage of the most critical functions',
+      'Orchestrated code deployment of Firebase Functions and sites for User Acceptance Testing',
+    ],
+  },
+  {
+    id: 'PBG',
+    company: 'Porter Brook Group',
+    title: 'Full Stack Developer',
+    dates: { start: '2011-06-01', end: '2018-11-01' },
+    highlights: [
+      'Spearheaded greenfield social, eCommerce, and IoT solutions for local and national businesses',
+      'Implemented DevOps, Agile, and Kanban project management methodologies',
+      'Advocated for an API-first approach in G2B, B2B, and B2C development',
+      'Utilized Linux-based cloud hosting on AWS EC2',
+      'Successfully integrated payment APIs from Stripe and Authorize.net',
+      'Created IoT-based connectivity microservice using Express.js, MariaDB, and PHP',
+    ],
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import AboutMe from './components/about-me';
+import MiniResume from './components/mini-resume';
 import MainNav from './components/nav';
+import { jobsData } from './data';
+import { Job } from './interfaces';
 
 export default function Home() {
   return (
     <>
       <MainNav></MainNav>
       <AboutMe></AboutMe>
+      <MiniResume jobs={jobsData as Job[]}></MiniResume>
     </>
   );
 }
