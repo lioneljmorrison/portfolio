b6acc945f6d619dd98454c409c7fd6d6c6660d5a
Lionel <lionel@morrison.mobi>
2023-08-09 11:23:03 -0400

    Added current skills

--- app/components/about-me.tsx
+++ app/components/about-me.tsx
@@ -29,6 +29,15 @@ export default function AboutMe() {
               <path d="M448 296c0 66.3-53.7 120-120 120h-8c-17.7 0-32-14.3-32-32s14.3-32 32-32h8c30.9 0 56-25.1 56-56v-8H320c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64h64c35.3 0 64 28.7 64 64v32 32 72zm-256 0c0 66.3-53.7 120-120 120H64c-17.7 0-32-14.3-32-32s14.3-32 32-32h8c30.9 0 56-25.1 56-56v-8H64c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64h64c35.3 0 64 28.7 64 64v32 32 72z" />
             </svg>
           </div>
+          <div className="pt-3 text-lg text-slate-300">Technologies I've been working with recently</div>
+          <div className="pt-3 flex flex-row space-x-2 text-xs">
+            <div className="py-1 px-2 bg-gray-900 rounded">SFCC SFRA</div>
+            <div className="py-1 px-2 bg-gray-900 rounded">TypeScript</div>
+            <div className="py-1 px-2 bg-gray-900 rounded">Tailwind CSS</div>
+            <div className="py-1 px-2 bg-gray-900 rounded">Nest.js</div>
+            <div className="py-1 px-2 bg-gray-900 rounded">Next.js</div>
+            <div className="py-1 px-2 bg-gray-900 rounded">React</div>
+          </div>
         </div>
       </div>
     </div>
