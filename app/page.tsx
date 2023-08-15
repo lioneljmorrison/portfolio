6ca0d17c272c4383a6dce1f2ba9aa315673168ad
Lionel Morrison <lionel@morrison.mobi>
2023-08-15 15:39:02 -0400

    Adjusted Nav and About

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,9 @@ export default function Home() {
   return (
     <>
       <MainNav data={navData}></MainNav>
-      <div className="w-auto p-4 md:p-6 bg-gray-700">
+      {/* <div className="w-auto p-4 md:p-6 bg-gray-700"> */}
+      <div className="max-w-7xl mx-auto bg-gray-700">
+        {/* <div className="max-w-7xl mx-auto py-4 px-2 sm:px-6 lg:px-8 bg-gray-700"> */}
         <AboutMe anchor="about" data={aboutMe}></AboutMe>
         <Experience anchor="experience" jobs={jobsData as Job[]}></Experience>
         <Projects anchor="projects" data={projectData}></Projects>
